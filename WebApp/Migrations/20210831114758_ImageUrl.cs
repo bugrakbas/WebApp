b1478fdaaa9932bf1b7c6ea70e6e2528ab6da872
bugrahanakbas <bugrahanakbs@gmail.com>
2021-09-01 09:33:31 +0300

    Add project files.

---
+++ WebApp/Migrations/20210831114758_ImageUrl.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApp.Migrations
+{
+    public partial class ImageUrl : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "TvShow",
+                maxLength: 60,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Rating",
+                table: "TvShow",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18,)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ImdbUrl",
+                table: "TvShow",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl",
+                table: "TvShow",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageUrl",
+                table: "TvShow");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "TvShow",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 60);
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Rating",
+                table: "TvShow",
+                type: "decimal(18,)",
+                nullable: false,
+                oldClrType: typeof(decimal));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ImdbUrl",
+                table: "TvShow",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string));
+        }
+    }
+}
---
+++ WebApp/Models/TvShow.cs
@@ -0,0 +1,44 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.Models
+{
+    public class TvShow
+    {
+
+        public int Id { get; set; }
+        public string Isim = "Dizi İzleme Linki";
+        public string Link = "Imdb Linki";
+        [Required]
+        [StringLength(60,MinimumLength =3)]
+        [Display(Name ="Başlık")]
+        public string Title { get; set; }
+        [Required]
+        [Display(Name ="Tür")]
+        public Genre Genre { get; set; }
+        [Required]
+        [DisplayFormat(DataFormatString ="{0:0.0#}")]
+        [Display(Name ="Reyting")]
+        public decimal Rating { get; set; }
+        [Required]
+        [DataType(DataType.Url)]
+        [Display(Name ="Imdb Link")]
+        public string ImdbUrl { get; set; }
+        [Required]
+        [DataType(DataType.ImageUrl)]
+        [Display(Name ="Resim")]
+        public string ImageUrl { get; set; }
+        [Required]
+        [DataType(DataType.Url)]
+        [Display(Name ="Diziyi İzle")]
+        public string WatchMovieUrl { get; set; }
+
+
+
+
+    }
+}
